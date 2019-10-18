4b2c3a3546891c16dc6c5618397f38121497f1d5
acharl <amadeo@fermat.org>
2019-10-18 10:29:51 +0200

    fix(delegation): displays delegation card if ktaddress is delegated

--- src/app/services/account-single/account-single.service.ts
+++ src/app/services/account-single/account-single.service.ts
@@ -95,12 +95,21 @@ export class AccountSingleService extends Facade<AccountSingleServiceState> {
       } else {
         this.apiService.getManagerAccount(address, 10).subscribe((managerAccounts: Account[]) => {
           const originAccounts: Account[] = []
+          const delegatedAccounts: Account[] = []
+          if (managerAccounts[0].delegate_value) {
+            delegatedAccounts.push(managerAccounts[0])
+          }
           managerAccounts.forEach(account => {
             if (account.manager) {
               this.apiService.getAccountById(account.manager).subscribe((accounts: Account[]) => {
                 if (accounts[0].account_id) {
                   originAccounts.push(accounts[0])
-                  this.updateState({ ...this._state, originatedAccounts: originAccounts, loading: false })
+                  this.updateState({
+                    ...this._state,
+                    delegatedAccounts: delegatedAccounts,
+                    originatedAccounts: originAccounts,
+                    loading: false
+                  })
                 }
               })
             }
