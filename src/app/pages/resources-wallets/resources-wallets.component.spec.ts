7f95391b315ce7431503fd9a52d6d60d955ade85
Pascal Brun <brunpasc@gmail.com>
2019-10-24 11:13:40 +0200

    feat(resources-wallets): fix tests

--- src/app/pages/resources-wallets/resources-wallets.component.spec.ts
+++ src/app/pages/resources-wallets/resources-wallets.component.spec.ts
@@ -1,25 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
-import { ResourcesWalletsComponent } from './resources-wallets.component';
+import { ResourcesWalletsComponent } from './resources-wallets.component'
+import { IconPipe } from 'src/app/pipes/icon/icon.pipe'
 
 describe('ResourcesWalletsComponent', () => {
-  let component: ResourcesWalletsComponent;
-  let fixture: ComponentFixture<ResourcesWalletsComponent>;
+  let component: ResourcesWalletsComponent
+  let fixture: ComponentFixture<ResourcesWalletsComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResourcesWalletsComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [ResourcesWalletsComponent, FontAwesomeModule, IconPipe]
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ResourcesWalletsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ResourcesWalletsComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
