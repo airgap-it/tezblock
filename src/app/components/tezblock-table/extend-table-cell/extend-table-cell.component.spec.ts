d042e6dc299762d398fe6e9b9e37c380e1451c7b
Pascal Brun <brunpasc@gmail.com>
2019-10-28 13:31:57 +0100

    fix(extend-table-cell): fixes test

--- src/app/components/tezblock-table/extend-table-cell/extend-table-cell.component.spec.ts
+++ src/app/components/tezblock-table/extend-table-cell/extend-table-cell.component.spec.ts
@@ -1,25 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { IconPipe } from 'src/app/pipes/icon/icon.pipe'
+import { UnitHelper } from 'test-config/unit-test-helper'
 
-import { ExtendTableCellComponent } from './extend-table-cell.component';
+import { ExtendTableCellComponent } from './extend-table-cell.component'
 
 describe('ExtendTableCellComponent', () => {
-  let component: ExtendTableCellComponent;
-  let fixture: ComponentFixture<ExtendTableCellComponent>;
+  let component: ExtendTableCellComponent
+  let fixture: ComponentFixture<ExtendTableCellComponent>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ExtendTableCellComponent ]
-    })
-    .compileComponents();
-  }));
+  let unitHelper: UnitHelper
+  beforeEach(() => {
+    unitHelper = new UnitHelper()
+    TestBed.configureTestingModule(
+      unitHelper.testBed({
+        providers: [IconPipe],
+        imports: [FontAwesomeModule],
+        declarations: [ExtendTableCellComponent]
+      })
+    )
+      .compileComponents()
+      .catch(console.error)
+    fixture = TestBed.createComponent(ExtendTableCellComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ExtendTableCellComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ExtendTableCellComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
