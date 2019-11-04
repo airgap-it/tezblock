c40f162fb4d6535fa29b784eaf40cbac9b5db9fa
Tim Schluchter <tim.schluchter@uzh.ch>
2019-11-04 16:42:10 +0100

    fix(environment.prod)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
+  mainnet: { rcp: '', conseil: '' },
+  babylonnet: { rcp: '', conseil: '' },
   production: true,
   conseilBaseUrl: 'CONSEIL_BASE_URL',
   conseilApiKey: 'CONSEIL_API_KEY',
-  proFontAwesomeAvailable: false,
-  mainnet: {},
-  babylonnet: {}
+  proFontAwesomeAvailable: false
 }
