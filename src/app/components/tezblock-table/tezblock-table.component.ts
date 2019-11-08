bee3e0c3b3a1e1f227c2c5410dc601d436826ddf
Pascal Brun <brunpasc@gmail.com>
2019-11-08 09:58:00 +0100

    feat(tezblock-table): renaming value to amount & refacotring

--- src/app/components/tezblock-table/tezblock-table.component.ts
+++ src/app/components/tezblock-table/tezblock-table.component.ts
@@ -24,7 +24,7 @@ import { HashCellComponent } from './hash-cell/hash-cell.component'
 import { PlainValueCellComponent } from './plain-value-cell/plain-value-cell.component'
 import { SymbolCellComponent } from './symbol-cell/symbol-cell.component'
 import { TimestampCellComponent } from './timestamp-cell/timestamp-cell.component'
-import { ModalCellComponent } from './modal-cell/modal-cell.component';
+import { ModalCellComponent } from './modal-cell/modal-cell.component'
 
 interface Column {
   name: string
@@ -111,7 +111,7 @@ const layouts: Layout = {
         options: { showFullAddress: false, pageId: 'oo' }
       },
       { name: 'Age', property: 'timestamp', width: '', component: TimestampCellComponent },
-      { name: 'Value', property: 'amount', width: '', component: AmountCellComponent },
+      { name: 'Amount', property: 'amount', width: '', component: AmountCellComponent },
       { name: 'Fee', property: 'fee', width: '', component: AmountCellComponent, options: { showFiatValue: false } },
       ...baseTx
     ],
@@ -134,7 +134,7 @@ const layouts: Layout = {
         transform: value => value || 'undelegate'
       },
       { name: 'Age', property: 'timestamp', width: '', component: TimestampCellComponent },
-      { name: 'Value', property: 'delegatedBalance', width: '', component: AmountCellComponent },
+      { name: 'Amount', property: 'delegatedBalance', width: '', component: AmountCellComponent },
       ...baseTx
     ],
     [OperationTypes.Origination]: [
@@ -238,17 +238,15 @@ const layouts: Layout = {
         component: AddressCellComponent,
         options: { showFullAddress: false, pageId: 'oo' }
       },
-      { name: 'Value', property: 'amount', width: '', component: AmountCellComponent },
+      { name: 'Amount', property: 'amount', width: '', component: AmountCellComponent },
       { name: 'Fee', property: 'fee', width: '', component: AmountCellComponent, options: { showFiatValue: false } },
       { name: 'Gas Limit', property: 'gas_limit', width: '' },
       { name: 'Tx Hash', property: 'operation_group_hash', width: '', component: HashCellComponent }
     ],
-
-    // TODO
     [OperationTypes.Overview]: [
       { name: 'Baker', property: 'baker', width: '1', component: AddressCellComponent, options: { showFullAddress: false, pageId: 'oo' } },
       { name: 'Age', property: 'timestamp', width: '', component: TimestampCellComponent },
-      { name: 'Value', property: 'volume', width: '', component: AmountCellComponent },
+      { name: 'Transaction Volume', property: 'volume', width: '', component: AmountCellComponent },
       { name: 'Fee', property: 'fee', width: '', component: AmountCellComponent, options: { showFiatValue: false } },
       { name: 'Transactions', property: 'txcount', width: '' },
       { name: 'Fitness', property: 'fitness', width: '' },
@@ -272,7 +270,7 @@ const layouts: Layout = {
         optionsTransform: (value, options) => (!value ? { ...options, isText: true } : options),
         transform: value => value || 'undelegate'
       },
-      { name: 'Value', property: 'delegatedBalance', width: '', component: AmountCellComponent },
+      { name: 'Amount', property: 'delegatedBalance', width: '', component: AmountCellComponent },
       { name: 'Fee', property: 'fee', width: '', component: AmountCellComponent, options: { showFiatValue: false } },
       { name: 'Gas Limit', property: 'gas_limit', width: '' },
       { name: 'Tx Hash', property: 'operation_group_hash', width: '', component: HashCellComponent }
@@ -340,7 +338,7 @@ const layouts: Layout = {
         component: AddressCellComponent,
         options: { showFullAddress: false, pageId: 'oo' }
       },
-      { name: 'Value', property: 'amount', width: '', component: AmountCellComponent },
+      { name: 'Amount', property: 'amount', width: '', component: AmountCellComponent },
       { name: 'Fee', property: 'fee', width: '', component: AmountCellComponent, options: { showFiatValue: false } },
       { name: 'Gas Limit', property: 'gas_limit', width: '' },
       { name: 'Storage Limit', property: 'storage_limit', width: '' },
@@ -360,7 +358,7 @@ const layouts: Layout = {
       },
       { name: 'Age', property: 'timestamp', width: '', component: TimestampCellComponent },
 
-      { name: 'Value', property: 'amount', width: '', component: AmountCellComponent },
+      { name: 'Amount', property: 'amount', width: '', component: AmountCellComponent },
       { name: 'Fees', property: 'fee', width: '', component: AmountCellComponent, options: { showFiatValue: false } },
       { name: 'Parameters', property: 'parameters', width: '', component: ModalCellComponent },
       { name: 'Block', property: 'block_level', width: '', component: BlockCellComponent },
