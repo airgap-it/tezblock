7fa1d402f806b98df376f0ba6052b268b5d3f452
Tim Schluchter <tim.schluchter@uzh.ch>
2019-11-18 14:35:07 +0100

    fix(tezblock-table.component.spec): fixed test

--- src/app/components/tezblock-table/tezblock-table.component.spec.ts
+++ src/app/components/tezblock-table/tezblock-table.component.spec.ts
@@ -1,12 +1,14 @@
 import { AddressCellComponent } from './address-cell/address-cell.component'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { TezblockTableComponent } from './tezblock-table.component'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { IconPipe } from 'src/app/pipes/icon/icon.pipe'
 import { UnitHelper } from 'test-config/unit-test-helper'
-import { LoadingSkeletonComponent } from '../loading-skeleton/loading-skeleton.component'
-import { AmountCellComponent } from './amount-cell/amount-cell.component'
 import { AddressItemComponent } from '../address-item/address-item.component'
 import { IdenticonComponent } from '../identicon/identicon'
+import { LoadingSkeletonComponent } from '../loading-skeleton/loading-skeleton.component'
+import { AmountCellComponent } from './amount-cell/amount-cell.component'
+import { TezblockTableComponent } from './tezblock-table.component'
 
 describe('TezblockTableComponent', () => {
   let component: TezblockTableComponent
@@ -17,7 +19,8 @@ describe('TezblockTableComponent', () => {
     unitHelper = new UnitHelper()
     TestBed.configureTestingModule(
       unitHelper.testBed({
-        providers: [],
+        providers: [IconPipe],
+        imports: [FontAwesomeModule],
         declarations: [
           TezblockTableComponent,
           LoadingSkeletonComponent,
