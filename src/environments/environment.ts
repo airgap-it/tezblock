6beabe4094108b3f332d9e3c09ecd187d85e067e
Tim Schluchter <tim.schluchter@uzh.ch>
2019-11-19 08:38:44 +0100

    fix(environment): fixed target url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,20 +8,20 @@ export const environment = {
     rpcUrl: 'MAINNET_RPC_URL',
     conseilUrl: 'MAINNET_CONSEIL_URL',
     conseilApiKey: 'MAINNET_CONSEIL_API_KEY',
-    targetUrl: 'https://' + 'TEZBLOCK_URL'
+    targetUrl: 'MAINNET_TARGET_URL'
   },
   carthagenet: {
     rpcUrl: 'CARTHAGENET_RPC_URL',
     conseilUrl: 'CARTHAGENET_CONSEIL_URL',
     conseilApiKey: 'CARTHAGENET_CONSEIL_API_KEY',
-    targetUrl: 'https://carthagenet.' + 'TEZBLOCK_URL'
+    targetUrl: 'CARTHAGENET_TARGET_URL'
   },
 
   babylonnet: {
     rpcUrl: 'BABYLONNET_RPC_URL',
     conseilUrl: 'BABYLONNET_CONSEIL_URL',
     conseilApiKey: 'BABYLONNET_CONSEIL_API_KEY',
-    targetUrl: 'https://babylonnet.' + 'TEZBLOCK_URL'
+    targetUrl: 'BABYLONNET_TARGET_URL'
   },
   conseilBaseUrl: 'CONSEIL_BASE_URL',
   proFontAwesomeAvailable: false
