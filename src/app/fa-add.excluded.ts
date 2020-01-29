a5811c4d8e0c2b60b6964bb74fa5529328fbb844
Tim Schluchter <tim.schluchter@uzh.ch>
2020-01-29 10:45:54 +0100

    reset env

---
+++ src/app/fa-add.excluded.ts
@@ -0,0 +1,63 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faMedium, faTelegram, faTwitter, faAndroid, faApple } from '@fortawesome/free-brands-svg-icons'
+import {
+  faBell,
+  faBoxBallot,
+  faCopy,
+  faExchangeAlt,
+  faEye,
+  faHandHoldingSeedling,
+  faHandReceiving,
+  faLevelDownAlt,
+  faLevelUpAlt,
+  faLink,
+  faLongArrowAltDown,
+  faQrcode,
+  faSearch,
+  faStamp,
+  faGlobe,
+  faLaptop,
+  faCaretUp,
+  faCaretDown,
+  faBreadLoaf,
+  faCoin,
+  faCoins,
+  faHatChef,
+  faListAlt,
+  faInfoCircle
+} from '@fortawesome/pro-light-svg-icons'
+
+export const addFontAwesome = () => {
+  library.add(
+    faBell,
+    faCopy,
+    faLevelDownAlt,
+    faLevelUpAlt,
+    faLongArrowAltDown,
+    faQrcode,
+    faSearch,
+    faExchangeAlt,
+    faHandReceiving,
+    faLink,
+    faStamp,
+    faBoxBallot,
+    faHandHoldingSeedling,
+    faEye,
+    faGithub,
+    faMedium,
+    faTelegram,
+    faTwitter,
+    faApple,
+    faAndroid,
+    faLaptop,
+    faGlobe,
+    faCaretUp,
+    faCaretDown,
+    faBreadLoaf,
+    faCoin,
+    faCoins,
+    faHatChef,
+    faListAlt,
+    faInfoCircle
+  )
+}
