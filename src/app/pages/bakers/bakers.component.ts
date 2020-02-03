163751c3ace83602e80b7c8b0382aec3e5eae918
Rafal Rapcewicz <r.rapcewicz@papers.ch>
2020-02-03 10:19:34 +0000

    feat(list--bakers-pie-chart): mobile and tooltip changes

--- src/app/pages/bakers/bakers.component.ts
+++ src/app/pages/bakers/bakers.component.ts
@@ -3,9 +3,9 @@ import { Observable, of, from } from 'rxjs'
 import { filter, map } from 'rxjs/operators'
 import { Store } from '@ngrx/store'
 import { Actions, ofType } from '@ngrx/effects'
-import { ChartOptions, ChartSize } from 'chart.js'
+import { ChartOptions, ChartSize, ChartTooltipItem, ChartData } from 'chart.js'
 import 'chartjs-plugin-piechart-outlabels'
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
 
 import { BaseComponent } from '@tezblock/components/base.component'
 import { Column } from '@tezblock/components/tezblock-table/tezblock-table.component'
@@ -15,6 +15,7 @@ import * as actions from './actions'
 import { othersBakersLabel } from './reducer'
 import { getRefresh } from '@tezblock/domain/synchronization'
 import { AliasPipe } from '@tezblock/pipes/alias/alias.pipe'
+import { AmountConverterPipe } from '@tezblock/pipes/amount-converter/amount-converter.pipe'
 import { Baker } from '@tezblock/services/api/api.service'
 
 const labelsParams = {
@@ -25,7 +26,7 @@ const labelsParams = {
   font: {
     resizable: true,
     minSize: 8,
-    maxSize: 12
+    maxSize: 11
   }
 }
 
@@ -72,25 +73,18 @@ export class BakersComponent extends BaseComponent implements OnInit {
   totalActiveBakers$: Observable<number>
   top24ChartDatasets$: Observable<{ data: number[]; label: string }[]>
   top24ChartLabels$: Observable<string[]>
-  top24ChartOptions: ChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        top: 130,
-        right: 150,
-        bottom: 170,
-        left: 150
-      }
-    },
-    plugins: {
-      outlabels: labelsParams
-    }
-  }
+  top24ChartOptions$: Observable<ChartOptions>
   top24ChartColors: any[] = [{ backgroundColor: palette }]
-  top24ChartSize: ChartSize = { width: 800, height: 600 }
+  top24ChartSize$: Observable<ChartSize>
+  isMobile$: Observable<boolean>
 
-  constructor(private readonly actions$: Actions, private readonly aliasPipe: AliasPipe, private readonly store$: Store<fromRoot.State>) {
+  constructor(
+    private readonly actions$: Actions,
+    private readonly aliasPipe: AliasPipe,
+    private readonly amountConverterPipe: AmountConverterPipe,
+    private readonly breakpointObserver: BreakpointObserver,
+    private readonly store$: Store<fromRoot.State>
+  ) {
     super()
   }
 
@@ -119,6 +113,11 @@ export class BakersComponent extends BaseComponent implements OnInit {
         ),
         map((labels: string[]) => labels.map(label => `${label} -`))
       )
+    this.isMobile$ = this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.Handset])
+      .pipe(map(breakpointState => breakpointState.matches))
+    this.top24ChartOptions$ = this.isMobile$.pipe(map(this.getOptions.bind(this)))
+    this.top24ChartSize$ = this.isMobile$.pipe(map(isMobile => (isMobile ? { width: 200, height: 200 } : { width: 750, height: 440 })))
 
     this.subscriptions.push(
       getRefresh([
@@ -136,5 +135,41 @@ export class BakersComponent extends BaseComponent implements OnInit {
 
   loadMore() {
     this.store$.dispatch(actions.increasePageOfActiveBakers())
+  }
+
+  private getOptions(isMobile: boolean): ChartOptions {
+    return {
+      responsive: true,
+      maintainAspectRatio: false,
+      layout: {
+        padding: isMobile
+          ? 0
+          : {
+              top: 80,
+              right: 130,
+              bottom: 190,
+              left: 130
+            }
+      },
+      plugins: {
+        outlabels: isMobile ? { display: false } : labelsParams
+      },
+      tooltips: {
+        callbacks: {
+          label: (tooltipItem: ChartTooltipItem, data: ChartData) => {
+            const bakerName = (<string>data.labels[tooltipItem.index]).replace('-', ':')
+            const stakingBalance = <number>data.datasets[0].data[tooltipItem.index]
+            const stakingBalanceLabel = this.amountConverterPipe.transform(stakingBalance, {
+              protocolIdentifier: 'xtz',
+              maxDigits: 6,
+              fontSmall: true,
+              fontColor: true
+            })
+
+            return `${bakerName} ${stakingBalanceLabel} ꜩ`
+          }
+        }
+      }
+    }
   }
 }
