eee4fc76266207579b925253afcaa4b3b9489fe9
Tim Schluchter <tim.schluchter@uzh.ch>
2020-02-03 12:18:09 +0100

    feat(block-detail.component): implemented historic value

--- src/app/components/block-detail-wrapper/block-detail-wrapper.component.ts
+++ src/app/components/block-detail-wrapper/block-detail-wrapper.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router'
 import { Observable } from 'rxjs'
 import { Block } from 'src/app/interfaces/Block'
 import { CurrencyInfo } from 'src/app/services/crypto-prices/crypto-prices.service'
+import { ChartDataService } from '@tezblock/services/chartdata/chartdata.service'
+import BigNumber from 'bignumber.js'
 
 @Component({
   selector: 'block-detail-wrapper',
@@ -30,7 +32,56 @@ export class BlockDetailWrapperComponent implements OnInit {
   @Input()
   public isMainnet: boolean = true
 
-  constructor(private readonly route: ActivatedRoute) {}
+  public historicFiatAmount = new Map<String, number>()
 
-  public ngOnInit() {}
+  public enableComparison: boolean = false
+
+  public showOldValueVolume: boolean = false
+  public showOldValueFee: boolean = false
+
+  public tooltipClick(amount, kind) {
+    this.wrapperBlock$.subscribe((block: Block) => {
+      const timestamp = block.timestamp
+
+      let daysDifference = this.dayDifference(timestamp)
+      if (daysDifference >= 1) {
+        let date = new Date(timestamp)
+        this.chartDataService.fetchHourlyMarketPrices(2, date, 'USD').then(response => {
+          let oldValue = new BigNumber(response[1].close)
+          amount = new BigNumber(amount)
+          const result = amount.multipliedBy(oldValue).toNumber()
+          this.historicFiatAmount.set(kind, result)
+          if (kind === 'volume') {
+            this.showOldValueVolume = !this.showOldValueVolume
+          } else if (kind === 'fee') {
+            this.showOldValueFee = !this.showOldValueFee
+          }
+        })
+      }
+    })
+  }
+
+  constructor(private readonly route: ActivatedRoute, private readonly chartDataService: ChartDataService) {}
+
+  public ngOnInit() {
+    if (this.wrapperBlock$) {
+      this.wrapperBlock$.subscribe((block: Block) => {
+        const timestamp = block.timestamp
+        const difference = this.dayDifference(timestamp)
+        this.enableComparison = difference >= 1
+      })
+    }
+  }
+
+  public dayDifference(oldTimestamp): number {
+    let currentTimestamp = +new Date()
+    const difference = currentTimestamp - oldTimestamp
+    const daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24)
+
+    return daysDifference
+  }
+
+  public getHistoricFiatAmount(key: string): number {
+    return this.historicFiatAmount.get(key)
+  }
 }
