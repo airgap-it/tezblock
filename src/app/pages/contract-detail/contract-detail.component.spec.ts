57d269b3d11c2a5ddb158a25133636490c18aa0f
Rafal Rapcewicz <r.rapcewicz@papers.ch>
2020-02-10 20:29:37 +0000

    feat(proposal-detail--add-voting-periods-and-vote): tests

--- src/app/pages/contract-detail/contract-detail.component.spec.ts
+++ src/app/pages/contract-detail/contract-detail.component.spec.ts
@@ -7,6 +7,18 @@ import { AddressItemComponent } from './../../components/address-item/address-it
 import { IdenticonComponent } from '@tezblock/components/identicon/identicon'
 import { IconPipe } from 'src/app/pipes/icon/icon.pipe'
 import { AliasPipe } from '@tezblock/pipes/alias/alias.pipe'
+import { LoadingSkeletonComponent } from 'src/app/components/loading-skeleton/loading-skeleton.component'
+import { TezblockTableComponent } from '@tezblock/components/tezblock-table/tezblock-table.component'
+import { AddressCellComponent } from 'src/app/components/tezblock-table/address-cell/address-cell.component'
+import { AmountCellComponent } from 'src/app/components/tezblock-table/amount-cell/amount-cell.component'
+import { BlockCellComponent } from '@tezblock/components/tezblock-table/block-cell/block-cell.component'
+import { TooltipItemComponent } from 'src/app/components/tooltip-item/tooltip-item.component'
+import { ChartItemComponent } from '@tezblock/components/chart-item/chart-item.component'
+import { ClientSideTableComponent } from '@tezblock/components/client-side-table/client-side-table.component'
+import { SymbolCellComponent } from '@tezblock/components/tezblock-table/symbol-cell/symbol-cell.component'
+import { HashCellComponent } from '@tezblock/components/tezblock-table/hash-cell/hash-cell.component'
+import { ModalCellComponent } from '@tezblock/components/tezblock-table/modal-cell/modal-cell.component'
+import { ExtendTableCellComponent } from '@tezblock/components/tezblock-table/extend-table-cell/extend-table-cell.component'
 
 describe('ContractDetailComponent', () => {
   let component: ContractDetailComponent
@@ -18,7 +30,21 @@ describe('ContractDetailComponent', () => {
     TestBed.configureTestingModule(
       unitHelper.testBed({
         imports: [FontAwesomeModule],
-        declarations: [ContractDetailComponent, AddressItemComponent, IdenticonComponent],
+        declarations: [
+          ContractDetailComponent,
+          LoadingSkeletonComponent,
+          AddressItemComponent,
+          IdenticonComponent,
+          TezblockTableComponent,
+          AddressCellComponent,
+          AmountCellComponent,
+          BlockCellComponent,
+          TooltipItemComponent,
+          SymbolCellComponent,
+          HashCellComponent,
+          ModalCellComponent,
+          ExtendTableCellComponent
+        ],
         providers: [IconPipe, AliasPipe]
       })
     ).compileComponents()
