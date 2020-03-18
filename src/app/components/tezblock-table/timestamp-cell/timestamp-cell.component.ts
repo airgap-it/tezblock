d29afcf1dc3b500ca1a8661b06ce125c32e6d651
Tim Schluchter <tim.schluchter@uzh.ch>
2020-03-18 12:21:57 +0100

    fix(timestamp-cell.component): fixed hovering of timestamp

--- src/app/components/tezblock-table/timestamp-cell/timestamp-cell.component.ts
+++ src/app/components/tezblock-table/timestamp-cell/timestamp-cell.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-timestamp-cell',
   templateUrl: './timestamp-cell.component.html',
   styleUrls: ['./timestamp-cell.component.scss']
 })
-export class TimestampCellComponent {
-  @Input() data: any // TODO: any
+export class TimestampCellComponent implements OnInit {
+  public date: Date
+  @Input() data: number
+
+  ngOnInit() {
+    if (this.data) {
+      this.date = new Date(this.data)
+    }
+  }
 }
