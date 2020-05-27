ba50eb8ad7de623c65041aae076746669a6dde58
Tim Schluchter <tim.schluchter@uzh.ch>
2020-05-27 08:51:55 +0200

    feat(baker-overview.comopnent): implemented translate

--- src/app/pages/baker-overview/baker-overview.component.ts
+++ src/app/pages/baker-overview/baker-overview.component.ts
@@ -18,6 +18,7 @@ import { AliasPipe } from '@tezblock/pipes/alias/alias.pipe'
 import { AmountConverterPipe } from '@tezblock/pipes/amount-converter/amount-converter.pipe'
 import { Baker } from '@tezblock/services/api/api.service'
 import { OrderBy } from '@tezblock/services/base.service'
+import { TranslateService } from '@ngx-translate/core'
 
 const labelsParams = {
   display: true,
@@ -84,7 +85,8 @@ export class BakerOverviewComponent extends BaseComponent implements OnInit {
     private readonly aliasPipe: AliasPipe,
     private readonly amountConverterPipe: AmountConverterPipe,
     private readonly breakpointObserver: BreakpointObserver,
-    private readonly store$: Store<fromRoot.State>
+    private readonly store$: Store<fromRoot.State>,
+    private translateService: TranslateService
   ) {
     super()
   }
@@ -134,7 +136,7 @@ export class BakerOverviewComponent extends BaseComponent implements OnInit {
       })
     )
 
-    this.columns = columns
+    this.columns = columns(this.translateService)
   }
 
   loadMore() {
--- src/app/pages/baker-overview/table-definitions.ts
+++ src/app/pages/baker-overview/table-definitions.ts
@@ -1,32 +1,33 @@
 import { Column, Template } from '@tezblock/components/tezblock-table/tezblock-table.component'
 import { Block } from '@tezblock/interfaces/Block'
+import { TranslateService } from '@ngx-translate/core'
 
-export const columns: Column[] = [
+export const columns = (translate: TranslateService): Column[] => [
   {
-    name: 'Baker',
+    name: translate.instant('tezblock-table.baker-overview.baker'),
     field: 'pkh',
     template: Template.address
   },
   {
-    name: 'Balance',
+    name: translate.instant('tezblock-table.baker-overview.balance'),
     field: 'balance',
     template: Template.amount,
     data: (item: any) => ({ data: { amount: item.balance } }),
     sortable: true
   },
   {
-    name: '# of Votes',
+    name: translate.instant('tezblock-table.baker-overview.number-of-votes'),
     field: 'number_of_votes'
   },
   {
-    name: 'Staking Balance',
+    name: translate.instant('tezblock-table.baker-overview.staking-balance'),
     field: 'staking_balance',
     template: Template.amount,
     data: (item: any) => ({ data: { amount: item.staking_balance } }),
     sortable: true
   },
   {
-    name: '# of Delegators',
+    name: translate.instant('tezblock-table.baker-overview.number-of-delegators'),
     field: 'number_of_delegators'
   }
 ]
