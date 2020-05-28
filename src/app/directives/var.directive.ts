480b8e8ab3dc2010be12c15561596aa0b11c1212
Rafal Rapcewicz <r.rapcewicz@papers.ch>
2020-05-28 11:01:42 +0200

    feat(account-detail--USD-denomination-improvement): done

---
+++ src/app/directives/var.directive.ts
@@ -0,0 +1,35 @@
+// https://stackoverflow.com/questions/38582293/how-to-declare-a-variable-in-a-template-in-angular
+
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+interface Context<T> {
+    appVar: T;
+}
+
+@Directive({
+    // don't use 'ng' prefix since it's reserved for Angular
+    selector: '[appVar]',
+})
+export class VarDirective<T = unknown> {
+    // https://angular.io/guide/structural-directives#typing-the-directives-context
+    static ngTemplateContextGuard<T>(dir: VarDirective<T>, ctx: any): ctx is Context<T> {
+        return true;
+    }
+
+    private context?: Context<T>;
+
+    constructor(
+        private vcRef: ViewContainerRef,
+        private templateRef: TemplateRef<Context<T>>
+    ) {}
+
+    @Input()
+    set appVar(value: T) {
+        if (this.context) {
+            this.context.appVar = value;
+        } else {
+            this.context = { appVar: value };
+            this.vcRef.createEmbeddedView(this.templateRef, this.context);
+        }
+    }
+}
