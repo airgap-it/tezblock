8eb100f2c602fa8586b396c3342b5fcf6735fc73
Tim Schluchter <tim.schluchter@uzh.ch>
2020-06-09 13:28:16 +0200

    feat(header-item.component): implemented switch for translation

---
+++ src/app/services/translation/load-languages.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
+import English from './../../../assets/i18n/en.json'
+import German from './../../../assets/i18n/de.json'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadLanguagesService {
+  supportedLanguages = ['en', 'de', 'zh-cn']
+
+  languages: { [key: string]: JSON } = {
+    en: English,
+    de: German
+  }
+
+  constructor(private readonly translate: TranslateService) {}
+
+  loadLanguages(translateTo?: string) {
+    let language = 'en'
+    translateTo
+      ? this.supportedLanguages.includes(translateTo.toLowerCase())
+        ? (language = translateTo.toLowerCase())
+        : (language = this.translate.getBrowserLang())
+      : (language = this.translate.getBrowserLang())
+
+    this.translate.setTranslation(language, this.languages[language])
+    this.translate.setDefaultLang('en')
+
+    if (language) {
+      const lowerCaseLanguage = language.toLowerCase()
+      this.supportedLanguages.forEach(supportedLanguage => {
+        if (supportedLanguage.startsWith(lowerCaseLanguage)) {
+          this.translate.use(supportedLanguage)
+        }
+      })
+    }
+  }
+}
