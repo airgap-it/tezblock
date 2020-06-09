16cadd8ea54c31cfbc5f8fced68caa6ebfea228a
Rafal Rapcewicz <r.rapcewicz@papers.ch>
2020-06-09 10:49:34 +0200

    feat(Make-tests-work-again): another portion

--- src/app/pages/account-overview/account-overview.component.spec.ts
+++ src/app/pages/account-overview/account-overview.component.spec.ts
@@ -1,53 +1,139 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideMockStore, MockStore } from '@ngrx/store/testing'
+import { TestScheduler } from 'rxjs/testing'
+import { Actions } from '@ngrx/effects'
+import { EMPTY, of } from 'rxjs'
+import { BreakpointObserver } from '@angular/cdk/layout'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 
 import { AccountOverviewComponent } from './account-overview.component'
-import { LoadingSkeletonComponent } from '@tezblock/components/loading-skeleton/loading-skeleton.component'
-import { ChartItemComponent } from '@tezblock/components/chart-item/chart-item.component'
-import { TezblockTableComponent } from '@tezblock/components/tezblock-table/tezblock-table.component'
-import { IdenticonComponent } from '@tezblock/components/identicon/identicon'
-import { AccountItemComponent } from '@tezblock/components/account-item/account-item.component'
-import { AddressItemComponent } from '@tezblock/components/address-item/address-item.component'
-import { AmountCellComponent } from '@tezblock/components/tezblock-table/amount-cell/amount-cell.component'
-import { BlockCellComponent } from '@tezblock/components/tezblock-table/block-cell/block-cell.component'
-import { SymbolCellComponent } from '@tezblock/components/tezblock-table/symbol-cell/symbol-cell.component'
-import { HashCellComponent } from '@tezblock/components/tezblock-table/hash-cell/hash-cell.component'
-import { ModalCellComponent } from '@tezblock/components/tezblock-table/modal-cell/modal-cell.component'
-import { ExtendTableCellComponent } from '@tezblock/components/tezblock-table/extend-table-cell/extend-table-cell.component'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { ChartsModule } from 'ng2-charts'
+import { initialState as aoInitialState } from './reducer'
+import { AliasPipe } from '@tezblock/pipes/alias/alias.pipe'
+import { getPipeMock } from 'test-config/mocks/pipe.mock'
+import { AmountConverterPipe } from '@tezblock/pipes/amount-converter/amount-converter.pipe'
+import { getBreakpointObserverMock, getObserveValue } from 'test-config/mocks/breakpoint-observer.mock'
+import { ShortenStringPipe } from '@tezblock/pipes/shorten-string/shorten-string.pipe'
 
-xdescribe('AccountOverviewComponent', () => {
+describe('AccountOverviewComponent', () => {
   let component: AccountOverviewComponent
   let fixture: ComponentFixture<AccountOverviewComponent>
+  let testScheduler: TestScheduler
+  let storeMock: MockStore<any>
+  const initialState = { accountsList: aoInitialState }
+  const aliasPipeMock = getPipeMock()
+  const amountConverterPipeMock = getPipeMock()
+  const breakpointObserverMock = getBreakpointObserverMock()
+  const shortenStringPipeMock = getPipeMock()
 
   beforeEach(async(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      // asserting the two objects are equal
+      expect(actual).toEqual(expected)
+    })
+
     TestBed.configureTestingModule({
-      declarations: [
-        AccountOverviewComponent,
-        LoadingSkeletonComponent,
-        ChartItemComponent,
-        TezblockTableComponent,
-        IdenticonComponent,
-        AccountItemComponent,
-        AddressItemComponent,
-        AmountCellComponent,
-        BlockCellComponent,
-        SymbolCellComponent,
-        HashCellComponent,
-        ModalCellComponent,
-        ExtendTableCellComponent
-      ],
-      imports: [FontAwesomeModule, ChartsModule]
-    }).compileComponents()
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [AccountOverviewComponent],
+      providers: [
+        provideMockStore({ initialState }),
+        { provide: Actions, useValue: EMPTY },
+        { provide: AliasPipe, useValue: aliasPipeMock },
+        { provide: AmountConverterPipe, useValue: amountConverterPipeMock },
+        { provide: BreakpointObserver, useValue: breakpointObserverMock },
+        { provide: ShortenStringPipe, useValue: shortenStringPipeMock }
+      ]
+    })
   }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AccountOverviewComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
+    storeMock = TestBed.inject(MockStore)
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  describe('ngOnInit', () => {
+
+    describe('top25ChartLabels$', () => {
+      beforeEach(() => {
+        component.ngOnInit()
+      })
+
+      it('when top25Accounts is NOT array then does not emit', () => {
+        testScheduler.run(helpers => {
+          const { expectObservable } = helpers
+          const expected = '---'
+  
+          expectObservable(component.top25ChartLabels$).toBe(expected)
+        })
+      })
+  
+      it('uses aliasPipe as main pipe', () => {
+        storeMock.setState({
+          ...initialState,
+          accountsList: {
+            ...initialState.accountsList,
+            top25Accounts: [
+              { account_id: '111' },
+              { account_id: '222' }
+            ]
+          }
+        })
+  
+        aliasPipeMock.transform.and.returnValue('AAA')
+        shortenStringPipeMock.transform.and.returnValue('BBB')
+  
+        testScheduler.run(helpers => {
+          const { expectObservable } = helpers
+          const expected = 'a'
+          const expectedValues = { a: ['AAA -', 'AAA -'] }
+  
+          expectObservable(component.top25ChartLabels$).toBe(expected, expectedValues)
+        })
+      })
+  
+      it('uses shortenStringPipe as secondary pipe', () => {
+        storeMock.setState({
+          ...initialState,
+          accountsList: {
+            ...initialState.accountsList,
+            top25Accounts: [
+              { account_id: '111' },
+              { account_id: '222' }
+            ]
+          }
+        })
+  
+        shortenStringPipeMock.transform.and.returnValue('BBB')
+  
+        testScheduler.run(helpers => {
+          const { expectObservable } = helpers
+          const expected = 'a'
+          const expectedValues = { a: ['BBB -', 'BBB -'] }
+  
+          expectObservable(component.top25ChartLabels$).toBe(expected, expectedValues)
+        })
+      })
+    })
+
+    // TODO: make it work
+    xdescribe('top25ChartSize$', () => {
+      it('when Breakpoints.Small, Breakpoints.Handset are emited returns small size', () => {
+        breakpointObserverMock.observe.and.returnValue(of(getObserveValue(true)))
+        component.ngOnInit()
+
+        testScheduler.run(helpers => {
+          const { expectObservable } = helpers
+          const expected = 'a'
+          const expectedValues = { a: { width: 200, height: 200 } }
+  
+          expectObservable(component.top25ChartLabels$).toBe(expected, expectedValues)
+        })
+      })
+    })
+    
   })
 })
