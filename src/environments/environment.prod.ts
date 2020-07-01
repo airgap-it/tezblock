69e59efd772cb6462b556cc9202a304c215730af
Tim Schluchter <tim.schluchter@uzh.ch>
2020-07-01 11:02:42 +0200

    chore(environment): removed links and keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
   mainnet: {
-    rpcUrl: 'https://tezos-node.prod.gke.papers.tech',
-    conseilUrl: 'https://tezos-mainnet-conseil-1.kubernetes.papers.tech',
-    conseilApiKey: 'airgap123',
-    targetUrl: 'https://tezblock.io'
+    rpcUrl: 'MAINNET_RPC_URL',
+    conseilUrl: 'MAINNET_CONSEIL_URL',
+    conseilApiKey: 'MAINNET_CONSEIL_API_KEY',
+    targetUrl: 'MAINNET_TARGET_URL'
   },
   carthagenet: {
-    rpcUrl: 'https://tezos-carthagenet-node-1.kubernetes.papers.tech',
-    conseilUrl: 'https://tezos-carthagenet-conseil-1.kubernetes.papers.tech',
-    conseilApiKey: 'airgap00391',
-    targetUrl: 'https://carthagenet.tezblock.io'
+    rpcUrl: 'CARTHAGENET_RPC_URL',
+    conseilUrl: 'CARTHAGENET_CONSEIL_URL',
+    conseilApiKey: 'CARTHAGENET_CONSEIL_API_KEY',
+    targetUrl: 'CARTHAGENET_TARGET_URL'
   },
   googleAnalyticsKey: undefined,
   proFontAwesomeAvailable: false
