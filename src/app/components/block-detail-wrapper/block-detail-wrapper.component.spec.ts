839db4d65c00d7b499d47b867dd55c4a39473bb9
Rafal Rapcewicz <r.rapcewicz@papers.ch>
2020-07-06 17:05:05 +0200

    feat(Make-tests-work-again): tests

--- src/app/components/block-detail-wrapper/block-detail-wrapper.component.spec.ts
+++ src/app/components/block-detail-wrapper/block-detail-wrapper.component.spec.ts
@@ -1,20 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core'
-
 import { BlockDetailWrapperComponent } from './block-detail-wrapper.component'
 
-xdescribe('BlockDetailWrapperComponent', () => {
+describe('BlockDetailWrapperComponent', () => {
   let component: BlockDetailWrapperComponent
-  let fixture: ComponentFixture<BlockDetailWrapperComponent>
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-      declarations: [BlockDetailWrapperComponent]
-    })
-
-    fixture = TestBed.createComponent(BlockDetailWrapperComponent)
-    component = fixture.componentInstance
+    component = new BlockDetailWrapperComponent()
   })
 
   it('should create', () => {
--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -1,32 +1,10 @@
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-
 import { FooterComponent } from './footer.component'
-import { UnitHelper } from 'test-config/unit-test-helper'
-import { IconPipe } from 'src/app/pipes/icon/icon.pipe'
 
-xdescribe('FooterComponent', () => {
+describe('FooterComponent', () => {
   let component: FooterComponent
-  let fixture: ComponentFixture<FooterComponent>
 
-  let unitHelper: UnitHelper
   beforeEach(() => {
-    unitHelper = new UnitHelper()
-
-    TestBed.configureTestingModule(
-      unitHelper.testBed({
-        providers: [IconPipe],
-        imports: [FontAwesomeModule],
-        declarations: [FooterComponent]
-      })
-    )
-      .compileComponents()
-      .catch(console.error)
-  })
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FooterComponent)
-    component = fixture.componentInstance
-    fixture.detectChanges()
+    component = new FooterComponent()
   })
 
   it('should create', () => {
