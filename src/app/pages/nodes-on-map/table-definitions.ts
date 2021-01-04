929ccbfc546c7d8c03cb7f71e58805f53c500024
Pascal Brun <p.brun@papers.ch>
2021-01-04 17:09:27 +0100

    Develop

---
+++ src/app/pages/nodes-on-map/table-definitions.ts
@@ -0,0 +1,29 @@
+import { Column } from '@tezblock/components/tezblock-table/tezblock-table.component'
+
+export const columns = (): Column[] => [
+  {
+    name: 'Country',
+    field: 'country',
+    sortable: true
+  },
+  {
+    name: 'Code',
+    field: 'countryCode',
+    sortable: true
+  },
+  {
+    name: 'City',
+    field: 'city',
+    sortable: true
+  },
+  {
+    name: 'Hosting',
+    field: 'org',
+    sortable: true
+  },
+  {
+    name: 'Neighbours',
+    field: 'neighbours',
+    sortable: true
+  }
+]
